93d15cff7a2fc2723e4022903ec8fbe43be7dffe
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-07-15 21:00:12 -0300

    including tableview

--- Bankey/Bankey/AppDelegate.swift
+++ Bankey/Bankey/AppDelegate.swift
@@ -29,7 +29,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         onboardingContainerViewController.delegate = self
         dummyViewController.logoutDelegate = self
         
-        window?.rootViewController = mainViewController
+        window?.rootViewController = AccountSummaryViewController()
         
         return true
     }
