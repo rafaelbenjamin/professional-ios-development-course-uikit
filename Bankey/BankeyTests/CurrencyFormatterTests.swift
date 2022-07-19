8e3a27acaf34f97c392cb08c93064f157a640811
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-07-18 21:39:28 -0300

    incluing unit tests for currencyformatter

---
+++ Bankey/BankeyTests/CurrencyFormatterTests.swift
@@ -0,0 +1,37 @@
+//
+//  CurrencyFormatterTests.swift
+//  BankeyTests
+//
+//  Created by Rafael Benjamin on 18/07/22.
+//
+
+import Foundation
+import XCTest
+
+@testable import Bankey
+
+class Test: XCTestCase {
+    
+    var formatter: CurrencyFormatter!
+    
+    override func setUp() {
+        super.setUp()
+        formatter = CurrencyFormatter()
+    }
+    
+    func testBreakDollarsIntoCents() throws {
+        let result = formatter.breakIntoDollarsAndCents(929466.23)
+        XCTAssertEqual(result.0, "929,466")
+        XCTAssertEqual(result.1, "23")
+    }
+    
+    func testDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(929466)
+        XCTAssertEqual(result, "$929,466.00")
+    }
+    
+    func testZeroDollarsFormatted() throws {
+        let result = formatter.dollarsFormatted(0)
+        XCTAssertEqual(result, "$0.00")
+    }
+}
