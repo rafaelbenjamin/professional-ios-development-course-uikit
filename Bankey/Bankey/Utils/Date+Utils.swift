52fbfc0986694bbe0cddc2963a18924605551170
Rafael Benjamin <rafaelb.90@hotmail.com>
2022-07-29 15:43:42 -0300

    add networking to AccountSummary

---
+++ Bankey/Bankey/Utils/Date+Utils.swift
@@ -0,0 +1,22 @@
+//
+//  Date+Utils.swift
+//  Bankey
+//
+//  Created by Rafael Benjamin on 29/07/22.
+//
+
+import Foundation
+
+extension Date {
+    static var bankeyDateFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.timeZone = TimeZone(abbreviation: "MDT")
+        return formatter
+    }
+    
+    var monthDayYearString: String {
+        let dateFormatter = Date.bankeyDateFormatter
+        dateFormatter.dateFormat = "MMM d, yyyy"
+        return dateFormatter.string(from: self)
+    }
+}
